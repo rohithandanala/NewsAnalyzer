43e38bdcf295e69ba232436e9036c5b460416ee4
RohithAndanala <andanala.rohith@gmail.com>
2025-05-31 00:29:14 -0400

    fixed bugs

--- Pipelines/run_prediction_on_file.py
+++ Pipelines/run_prediction_on_file.py
@@ -6,6 +6,7 @@ PATH = "/home/ec2-user/NewsBucketMount/news_data/trending_news.csv"
 
 def run_prediction():
     data = pd.read_csv(PATH)
+    data = data.dropna(subset=['summary']) 
 
     data['pred'] = data['summary'].apply(predict_text)
 
